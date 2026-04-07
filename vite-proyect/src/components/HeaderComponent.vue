4f9196553cfea711e1ecd3fb60c97ec662710842
NonoelqueTv <valentorcivia2005@gmail.com>
2026-04-07 14:26:58 -0300

    clase 4

---
+++ vite-proyect/src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<template>
+    <header>
+        <div>
+            <a> href="#" aria-label="patitas y croquetas, inicio"
+                <span aria-hidden="true">🐕‍🦺🐕‍🦺</span>
+                <span>Patitas & croquetas</span>
+                <span>Alimento para perros</span>
+            </a>
+            <nav>
+                <ul>
+                    <li>Aca va estar el nav</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style>
+</style>
